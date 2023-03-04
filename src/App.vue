6ac6a98edfd3069b10f2fe0f93a1ac03b3fba55a
Robert Medina <csoperativo@gmail.com>
2023-03-04 18:24:44 -0400

    splashScreen para la app

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <Suspense>
+        <template #default>
+            <Home />
+        </template>
+        <template #fallback>
+            <SplashScreen />
+        </template>
+    </Suspense>
+</template>
+
+<script>
+    import SplashScreen from "@/components/SplashScreen"
+    import { defineAsyncComponent } from "vue";
+
+    export default {
+        components: {
+            SplashScreen,
+            Home: defineAsyncComponent(()=> 
+                new Promise((resolve) => {
+                    setTimeout(() => {
+                        resolve(import("./components/Home"))
+                    }, 2500)
+                })
+            )
+        }
+    }
+</script>
+
+<style>
+    html, body, .app {
+        min-width: 100vh;
+        margin: 0;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    *{
+        --brand-green: #04b500;
+        --brand-blue: #0689b0;
+    }
+</style>
---
+++ src/components/SplashScreen/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="splashscreen">
+        <img src="@/assets/cashflow-icon.svg" alt="Logo" />
+        <h1>Personal<span>Finance</span> </h1>
+    </div>
+</template>
+
+<style scoped>
+    .splashscreen {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        height: 100vh;
+    }
+    h1{
+        color: var(--brand-blue);
+    }
+    h1 span {
+        color: var(--brand-green);
+    }
+</style>
