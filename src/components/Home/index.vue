c7e89acb77670d3ef7868c7277fcadeaf959738f
Robert Medina <csoperativo@gmail.com>
2023-03-04 23:00:27 -0400

    grafico listo reactivo

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -7,7 +7,7 @@
             <Resume 
             :label="label" :amount="amount" :totalAmount="100000" >
                 <template #graphic>
-                    graphhic
+                    <Graphic :amounts="amounts"/>
                 </template>
                 <template #action>
                     <Action /> 
@@ -26,18 +26,21 @@
     import Resume from "@/components/Resume"
     import Movements from "@/components/Movements"
     import Action from "@/components/Action"
+    import Graphic from "@/components/Graphic"
     export default {
         components: {
             Header,
             Layout,
             Resume,
             Movements,
-            Action
+            Action,
+            Graphic
         },
         data() {
             return {
                 amount: null,
                 label: null,
+                amounts: [100, 200, 500, 200, -600, 2400, -1000, 500],
                 movements: [{
                     id: 1,
                     title: "Movimiento Uno",
